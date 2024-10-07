47cba541bf22621fb62711cef6b620ec8b2dc1e9
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-07 14:22:24 +0200

    printed on the page the password

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/functions.php';
+var_dump($_GET['password_length']);
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +10,7 @@ require_once __DIR__ . '/functions.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Password Generator</title>
+    <!-- style -->
     <link rel="stylesheet" href="./style.css">
     <!-- bootstrap 5.3.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -27,6 +29,9 @@ require_once __DIR__ . '/functions.php';
                 Genera una password sicura
             </h2>
             <!-- se il parametro è vuoto -->
+            <?php
+            if (empty($_GET['password_length'])) {
+            ?>
             <div class="row">
                 <div class="col-12">
                     <p class="empty p-4">
@@ -34,14 +39,32 @@ require_once __DIR__ . '/functions.php';
                     </p>
                 </div>
             </div>
+            <?php
+            }
+            ?>
             <form action="" method="get" class="p-4">
                 <div class="row gy-3">
                     <div class="col-6">
-                        <label for="password-length">Lunghezza password:</label>
+                        <label for="password_length">Lunghezza password:</label>
                     </div>
                     <div class="col-6">
-                        <input type="number" id="password-length" name="password-length">
+                        <input type="number" id="password_length" name="password_length"
+                            placeholder="Inserisci il numero dei caratteri">
                     </div>
+                    <!-- se c'è e se non è vuoto -->
+                    <?php
+                    if (isset($_GET['password_length']) && !empty($_GET['password_length'])) {
+                    ?>
+                    <div class="col-12">
+                        <!-- stampa la password generata -->
+                        <p>
+                            La password generata è: <?php echo random_password(intval($_GET['password_length'])) ?>
+                        </p>
+
+                    </div>
+                    <?php
+                    }
+                    ?>
                     <div class="col-12">
                         <button type="submit" class="btn btn-primary">Invia</button>
                         <button type="reset" class="btn btn-secondary">Annulla</button>
