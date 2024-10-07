3fae90645807ecc4b2f149892dd15be9e5c1a213
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-07 13:35:53 +0200

    created form

--- index.php
+++ index.php
@@ -12,6 +12,40 @@
 </head>
 
 <body>
+    <div class="container">
+        <header>
+            <h1 class="text-center pt-5 mb-3">
+                Strong Password Generator
+            </h1>
+        </header>
+        <main>
+            <h2 class="text-white text-center mb-3">
+                Genera una password sicura
+            </h2>
+            <!-- se il parametro è vuoto -->
+            <div class="row">
+                <div class="col-12">
+                    <p class="empty p-4">
+                        Nessun parametro valido inserito
+                    </p>
+                </div>
+            </div>
+            <form action="" method="get" class="p-4">
+                <div class="row gy-3">
+                    <div class="col-6">
+                        <label for="password-length">Lunghezza password:</label>
+                    </div>
+                    <div class="col-6">
+                        <input type="number" id="password-length" name="password-length">
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Invia</button>
+                        <button type="reset" class="btn btn-secondary">Annulla</button>
+                    </div>
+                </div>
+            </form>
+        </main>
+    </div>
 
 </body>
 
